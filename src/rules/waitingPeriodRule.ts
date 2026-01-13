f70448dd6d40a26cb09e5b4059db0b2067c41b81
Anurag Singh <iamanuragsingh10@gmail.com>
2026-01-13 17:09:57 +0530

    changed waiting period logic

--- src/rules/waitingPeriodRule.ts
+++ src/rules/waitingPeriodRule.ts
@@ -1,9 +1,8 @@
 export function applyWaitingPeriodRule(plans: any[], hasDisease: boolean) {
-  if (!hasDisease) {
-    return plans;
-  }
-  return [...plans]
-    .sort((a, b) => a.waitingPeriodYears - b.waitingPeriodYears)
+  if (!hasDisease) return plans;
+
+  return plans
+    .filter(plan => plan.waitingPeriodYears <= 3)
     .map(plan => ({
       ...plan,
       reasons: [
